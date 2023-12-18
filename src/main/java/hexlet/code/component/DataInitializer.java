b45f8cdc764f6c80c59b8d42a3f2b5f560902906
Azeend <marat9466@gmail.com>
2023-12-18 21:32:23 +0300

    some fixes

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,39 +1,36 @@
 package hexlet.code.component;
 
-import hexlet.code.dto.UserCreateDto;
 import hexlet.code.model.Label;
 import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
 import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
 
-import hexlet.code.service.UserService;
-import io.sentry.Sentry;
+import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
-    private final UserService userService;
+
     private final TaskStatusRepository taskStatusRepository;
     private final LabelRepository labelRepository;
+    private final UserRepository userRepository;
+
+    private PasswordEncoder encoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        try {
-            throw new Exception("This is a test.");
-        } catch (Exception e) {
-            Sentry.captureException(e);
-        }
-
         var email = "hexlet@example.com";
-        var userData = new UserCreateDto();
+        var userData = new User();
         userData.setEmail(email);
-        userData.setPasswordDigest("qwerty");
-        userService.create(userData);
+        userData.setPasswordDigest(encoder.encode("qwerty"));
+        userRepository.save(userData);
 
 
         makeDefaultStatus("Draft", "draft");
