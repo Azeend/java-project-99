473983c5ec3741d6eb1e1ac719430e271c2fb528
Azeend <marat9466@gmail.com>
2023-12-31 13:54:03 +0300

    fix password

--- src/test/java/hexlet/code/util/ModelsGenerator.java
+++ src/test/java/hexlet/code/util/ModelsGenerator.java
@@ -31,7 +31,7 @@ public class ModelsGenerator {
         testUser = Instancio.of(User.class)
                 .ignore(Select.field(User::getId))
                 .ignore(Select.field(User::getTasks))
-                .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 10))
+                .supply(Select.field(User::getPassword), () -> faker.internet().password(3, 10))
                 .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
                 .supply(Select.field(User::getLastName), () -> faker.name().lastName())
                 .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
